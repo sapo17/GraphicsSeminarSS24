f3aee209f84c42ce87626acafeca3463a8d97be9
Saip Can Hasbay <saip.hasbay@siemens.com>
2024-04-04 13:40:24 +0200

    Add: bcc voronoi and delaunay

--- experiments/lattices.py
+++ experiments/lattices.py
@@ -100,7 +100,7 @@ def plot_cube_lattice(points, elev=30, azim=30):
                         [point[2], neighbor[2]],
                         linestyle="dotted",
                         color=lattice_grid_color,
-                        alpha=0.5
+                        alpha=0.5,
                     )
 
     # Define the cube at the center with adjusted transparency
@@ -158,3 +158,80 @@ def plot_cube_lattice(points, elev=30, azim=30):
 # Plot the cubic lattice points with a central transparent unit cube
 cubic_points = generate_cubic_lattice_points(3)
 plot_cube_lattice(cubic_points, elev=10, azim=25)
+
+import trimesh
+
+# Load the OBJ files
+file_path1 = "models/cube.obj"
+file_path2 = "models/truncated_octahedron.obj"
+
+cube = trimesh.load(file_path1)
+octahedron = trimesh.load(file_path2)
+
+# Define the colors from the GnBu colormap
+gnbu_colormap = plt.colormaps["GnBu"]
+lattice_grid_color = gnbu_colormap(0.2)  # Color for cube edges
+voronoi_color = gnbu_colormap(0.4)  # Color for octahedron
+point_color = gnbu_colormap(0.5)
+
+# Extracting the unique edges for the octahedron
+edges = octahedron.edges_unique
+edge_vertices = octahedron.vertices[edges]
+
+
+# Define a function to draw the octahedron faces without drawing each triangle
+def draw_polygons(ax, vertices, faces, color, alpha=0.05):
+    poly3d = [[vertices[vertex] for vertex in face] for face in faces]
+    ax.add_collection3d(
+        Poly3DCollection(
+            poly3d,
+            facecolors=color,
+            linewidths=1,
+            edgecolors=voronoi_color,
+            alpha=alpha,
+        )
+    )
+
+
+# Set up the plot
+fig = plt.figure(dpi=300)
+ax = fig.add_subplot(projection="3d")
+
+ax.plot_trisurf(
+    cube.vertices[:, 0],
+    cube.vertices[:, 1],
+    cube.vertices[:, 2],
+    triangles=cube.faces,
+    color=lattice_grid_color,
+    edgecolor=lattice_grid_color,
+    linestyle='dotted',
+    alpha=0
+)
+
+# Draw the polygons for the octahedron
+draw_polygons(ax, octahedron.vertices, octahedron.faces, voronoi_color)
+
+ax.scatter(0, 0, 0, color=point_color, s=50)
+
+# Plot the vertices
+ax.scatter(
+    octahedron.vertices[:, 0],
+    octahedron.vertices[:, 1],
+    octahedron.vertices[:, 2],
+    color=voronoi_color,
+    label="Voronoi Region of the site at the center"
+)
+
+ax.scatter(
+    cube.vertices[:, 0],
+    cube.vertices[:, 1],
+    cube.vertices[:, 2],
+    color=point_color,
+)
+
+plt.legend()
+
+# Setting the axis off and adjusting the layout
+ax.axis("off")
+plt.tight_layout()
+plt.show()
